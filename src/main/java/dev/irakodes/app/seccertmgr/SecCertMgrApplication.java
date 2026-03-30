9ad815b2887893158c37bfb786992942b84c3450
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2026-03-31 01:19:33 +0200

    Inception

---
+++ src/main/java/dev/irakodes/app/seccertmgr/SecCertMgrApplication.java
@@ -0,0 +1,13 @@
+package dev.irakodes.app.seccertmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecCertMgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecCertMgrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/irakodes/app/seccertmgr/SecCertMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.irakodes.app.seccertmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecCertMgrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
